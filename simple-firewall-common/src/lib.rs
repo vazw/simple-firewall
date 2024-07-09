2cb778bc3653eca695ebf62e3ddcf634ef923818
vazw <vaz@siamstr.com>
2024-07-10 00:31:55 +0700

    add default and correct connection data

--- simple-firewall-common/src/lib.rs
+++ simple-firewall-common/src/lib.rs
@@ -15,7 +15,7 @@ pub struct Connection {
 #[cfg(feature = "user")]
 unsafe impl aya::Pod for Connection {}
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Default)]
 pub struct ConnectionState {
     pub last_syn_ack_time: u64,
     pub syn_ack_count: u32,
@@ -30,7 +30,7 @@ pub struct ConnectionState {
 unsafe impl aya::Pod for ConnectionState {}
 
 impl ConnectionState {
-    pub fn to_backlist_key(self: &Self) -> Session {
+    pub fn to_backlist_key(&self) -> Session {
         Session {
             src_ip: self.remote_ip,
             src_port: self.remote_port,
@@ -39,6 +39,17 @@ impl ConnectionState {
         }
     }
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct IcmpPacket {
+    pub src_ip: u32,
+    pub dst_ip: u32,
+    pub icmp_type: u8,
+    pub icmp_code: u8,
+    pub checksum: u16,
+}
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for IcmpPacket {}
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
@@ -51,17 +62,6 @@ pub struct Session {
 
 #[cfg(feature = "user")]
 unsafe impl aya::Pod for Session {}
-
-#[derive(Clone, Copy, Debug)]
-pub struct IcmpPacket {
-    pub src_ip: u32,
-    pub dst_ip: u32,
-    pub icmp_type: u8,
-    pub icmp_code: u8,
-    pub checksum: u16,
-}
-#[cfg(feature = "user")]
-unsafe impl aya::Pod for IcmpPacket {}
 
 impl Session {
     #[inline(always)]
@@ -114,8 +114,32 @@ impl Session {
 }
 
 impl Connection {
+    //
+    // dst : REMOTE
+    //
     #[inline(always)]
     pub fn egress(
+        src_ip: u32,
+        src_port: u16,
+        dst_ip: u32,
+        dst_port: u16,
+        protocal: u8,
+    ) -> Self {
+        Connection {
+            src_ip,
+            dst_ip,
+            src_port,
+            dst_port,
+            protocal,
+            _padding: [0u8; 3],
+        }
+    }
+
+    //
+    // src : REMOTE
+    //
+    #[inline(always)]
+    pub fn ingress(
         src_ip: u32,
         src_port: u16,
         dst_ip: u32,
@@ -133,34 +157,18 @@ impl Connection {
     }
 
     #[inline(always)]
-    pub fn ingress(
-        src_ip: u32,
-        src_port: u16,
-        dst_ip: u32,
-        dst_port: u16,
-        protocal: u8,
-    ) -> Self {
-        Connection {
-            src_ip,
-            dst_ip,
-            src_port,
-            dst_port,
-            protocal,
-            _padding: [0u8; 3],
-        }
-    }
-
-    #[inline(always)]
-    pub fn into_session(self: &Self) -> Session {
+    pub fn into_session(&self) -> Session {
+        // USE OUR HOST AS SessionKey
         Session {
-            src_ip: self.dst_ip,
-            src_port: self.dst_port,
+            src_ip: self.src_ip,
+            src_port: self.src_port,
             protocal: self.protocal,
             _padding: self._padding[0],
         }
     }
     #[inline(always)]
-    pub fn into_state(self: &Self) -> ConnectionState {
+    pub fn into_state(&self) -> ConnectionState {
+        // USE REMOTE AS MAP VALUE
         ConnectionState {
             last_syn_ack_time: 0,
             syn_ack_count: 0,
@@ -172,7 +180,7 @@ impl Connection {
         }
     }
     #[inline(always)]
-    pub fn into_state_listen(self: &Self) -> ConnectionState {
+    pub fn into_state_listen(&self) -> ConnectionState {
         ConnectionState {
             last_syn_ack_time: 0,
             syn_ack_count: 0,
