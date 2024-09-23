f4c90835be16e8e4493c49c0b92b0a931ff5c554
vazw <vaz@siamstr.com>
2024-09-24 00:24:52 +0700

    convert 2

--- simple-firewall/src/main.rs
+++ simple-firewall/src/main.rs
@@ -216,7 +216,7 @@ async fn main() -> Result<(), anyhow::Error> {
                             conn.host_addr.into(),
                             conn.remote_port,
                             conn.host_port,
-                            conn.seq.to_be() -1,
+                            (conn.seq.to_le() -1).to_be(),
                             &mut syn_buf
                         )
                     } else if conn.tcp_flag.eq(&18) {
@@ -229,8 +229,8 @@ async fn main() -> Result<(), anyhow::Error> {
                             conn.host_addr.into(),
                             conn.remote_port,
                             conn.host_port,
-                            conn.ack_seq.to_be(),
-                            conn.seq.to_be()+1,
+                            conn.ack_seq,
+                            (conn.seq.to_le()+1).to_be(),
                             &mut ack_buf
                         )
 
