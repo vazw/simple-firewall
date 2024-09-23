4d2899375f3f0c7fe32e7495c1792b177543ce27
vazw <vaz@siamstr.com>
2024-09-23 16:26:27 +0700

    compress log & move struct to helper.rs

---
+++ simple-firewall/src/helper.rs
@@ -0,0 +1,204 @@
+use aya::maps::HashMap;
+use aya::Bpf;
+use clap::Parser;
+use log::info;
+use serde::Deserialize;
+use std::net::Ipv4Addr;
+#[derive(Debug, Clone, Deserialize)]
+pub struct TcpIn {
+    pub sport: Option<Vec<u16>>,
+    pub dport: Option<Vec<u16>>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TcpOut {
+    pub sport: Option<Vec<u16>>,
+    pub dport: Option<Vec<u16>>,
+}
+#[derive(Debug, Clone, Deserialize)]
+pub struct UdpIn {
+    pub sport: Option<Vec<u16>>,
+    pub dport: Option<Vec<u16>>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct UdpOut {
+    pub sport: Option<Vec<u16>>,
+    pub dport: Option<Vec<u16>>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct AppConfig {
+    pub dns: Option<Vec<String>>,
+    pub tcp_in: Option<TcpIn>,
+    pub tcp_out: Option<TcpOut>,
+    pub udp_in: Option<UdpIn>,
+    pub udp_out: Option<UdpOut>,
+}
+
+impl AppConfig {
+    pub fn len(&self) -> u16 {
+        let mut len: u16 = 0;
+        if let Some(dns) = &self.dns {
+            len += dns.len() as u16;
+        }
+        if let Some(tcp) = &self.tcp_in {
+            if let Some(n) = &tcp.sport {
+                len += n.len() as u16;
+            }
+            if let Some(n) = &tcp.dport {
+                len += n.len() as u16;
+            }
+        }
+        if let Some(tcp) = &self.tcp_out {
+            if let Some(n) = &tcp.sport {
+                len += n.len() as u16;
+            }
+            if let Some(n) = &tcp.dport {
+                len += n.len() as u16;
+            }
+        }
+        if let Some(udp) = &self.udp_in {
+            if let Some(n) = &udp.sport {
+                len += n.len() as u16;
+            }
+            if let Some(n) = &udp.dport {
+                len += n.len() as u16;
+            }
+        }
+        if let Some(udp) = &self.udp_out {
+            if let Some(n) = &udp.sport {
+                len += n.len() as u16;
+            }
+            if let Some(n) = &udp.dport {
+                len += n.len() as u16;
+            }
+        }
+        len
+    }
+    pub fn is_empty(&self) -> bool {
+        self.len().eq(&0)
+    }
+}
+
+#[derive(Debug, Parser)]
+pub struct Opt {
+    #[clap(short, long, default_value = "wlp1s0")]
+    pub iface: String,
+    #[clap(short, long, default_value = "/etc/sfw/sfwconfig.toml")]
+    pub config: String,
+}
+
+pub fn load_config(
+    bpf: &mut Bpf,
+    config: &AppConfig,
+) -> Result<(), anyhow::Error> {
+    if let Some(dns) = &config.dns {
+        let mut dns_list: HashMap<_, u32, u8> =
+            HashMap::try_from(bpf.map_mut("DNS_ADDR").unwrap())?;
+        for k in dns {
+            let ip_addrs: Ipv4Addr = k.parse().unwrap();
+            let addrs: u32 = ip_addrs.to_bits();
+            info!("allowed DNS IP: {:}", ip_addrs.to_string());
+            _ = dns_list.insert(addrs, 0x1, 0);
+        }
+    }
+    if let Some(tcp) = &config.tcp_in {
+        if let Some(n) = &tcp.sport {
+            let mut tcp_in_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("TCP_IN_SPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = tcp_in_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow incomming from tcp port: {:?}", port);
+                tcp_in_port.insert(port, 1u8, 0)?;
+            }
+        }
+        if let Some(n) = &tcp.dport {
+            let mut tcp_in_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("TCP_IN_DPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = tcp_in_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow incomming to tcp port: {:?}", port);
+                tcp_in_port.insert(port, 1u8, 0)?;
+            }
+        }
+    }
+    if let Some(tcp) = &config.tcp_out {
+        if let Some(n) = &tcp.sport {
+            let mut tcp_out_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("TCP_OUT_SPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = tcp_out_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow outgoing from tcp port: {:?}", port);
+                tcp_out_port.insert(port, 1u8, 0)?;
+            }
+        }
+        if let Some(n) = &tcp.dport {
+            let mut tcp_out_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("TCP_OUT_DPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = tcp_out_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow outgoing to tcp port: {:?}", port);
+                tcp_out_port.insert(port, 1u8, 0)?;
+            }
+        }
+    }
+    if let Some(udp) = &config.udp_in {
+        if let Some(n) = &udp.sport {
+            let mut udp_in_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("UDP_IN_SPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = udp_in_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow incomming from udp port {:?}", port);
+                udp_in_port.insert(port, 1u8, 0)?;
+            }
+        }
+        if let Some(n) = &udp.dport {
+            let mut udp_in_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("UDP_IN_DPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = udp_in_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow incomming to udp port {:?}", port);
+                udp_in_port.insert(port, 1u8, 0)?;
+            }
+        }
+    }
+    if let Some(udp) = &config.udp_out {
+        if let Some(n) = &udp.sport {
+            let mut udp_out_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("UDP_OUT_SPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = udp_out_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow outgoing from udp port {:?}", port);
+                udp_out_port.insert(port, 1u8, 0)?;
+            }
+        }
+        if let Some(n) = &udp.dport {
+            let mut udp_out_port: HashMap<_, u16, u8> =
+                HashMap::try_from(bpf.map_mut("UDP_OUT_DPORT").unwrap())?;
+            for port in 0..=65535 {
+                _ = udp_out_port.remove(&port);
+            }
+            for port in n {
+                info!("Allow outgoing to udp port {:?}", port);
+                udp_out_port.insert(port, 1u8, 0)?;
+            }
+        }
+    }
+    info!("Done parsing config!");
+    Ok(())
+}
--- simple-firewall/src/main.rs
+++ simple-firewall/src/main.rs
@@ -1,11 +1,14 @@
 #![allow(unreachable_code)]
-use std::net::Ipv4Addr;
+pub mod helper;
+use helper::*;
 
 use anyhow::Ok;
-use aya::maps::HashMap;
+use aya::maps::AsyncPerfEventArray;
 use aya::programs::{tc, SchedClassifier, TcAttachType, Xdp, XdpFlags};
+use aya::util::online_cpus;
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
+use bytes::BytesMut;
 use clap::Parser;
 use figment::{
     providers::{Format, Toml},
@@ -13,111 +16,61 @@ use figment::{
 };
 use log::LevelFilter;
 use log::{debug, info, warn};
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Config, Root};
+
+use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
+use log4rs::append::rolling_file::policy::compound::{
+    roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger,
+};
+use log4rs::append::rolling_file::RollingFileAppender;
+
 use log4rs::encode::pattern::PatternEncoder;
 
-use serde::Deserialize;
+use log4rs::config::{Appender, Root};
+use log4rs::Config;
+
+use simple_firewall_common::Connection;
+// use tokio::io::unix::AsyncFd;
 use tokio::signal;
 use tokio::time::{interval, Duration};
-
-#[derive(Debug, Clone, Deserialize)]
-struct TcpIn {
-    pub sport: Option<Vec<u16>>,
-    pub dport: Option<Vec<u16>>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct TcpOut {
-    pub sport: Option<Vec<u16>>,
-    pub dport: Option<Vec<u16>>,
-}
-#[derive(Debug, Clone, Deserialize)]
-struct UdpIn {
-    pub sport: Option<Vec<u16>>,
-    pub dport: Option<Vec<u16>>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct UdpOut {
-    pub sport: Option<Vec<u16>>,
-    pub dport: Option<Vec<u16>>,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-struct AppConfig {
-    pub dns: Option<Vec<String>>,
-    pub tcp_in: Option<TcpIn>,
-    pub tcp_out: Option<TcpOut>,
-    pub udp_in: Option<UdpIn>,
-    pub udp_out: Option<UdpOut>,
-}
-
-impl AppConfig {
-    pub fn len(&self) -> u16 {
-        let mut len: u16 = 0;
-        if let Some(dns) = &self.dns {
-            len += dns.len() as u16;
-        }
-        if let Some(tcp) = &self.tcp_in {
-            if let Some(n) = &tcp.sport {
-                len += n.len() as u16;
-            }
-            if let Some(n) = &tcp.dport {
-                len += n.len() as u16;
-            }
-        }
-        if let Some(tcp) = &self.tcp_out {
-            if let Some(n) = &tcp.sport {
-                len += n.len() as u16;
-            }
-            if let Some(n) = &tcp.dport {
-                len += n.len() as u16;
-            }
-        }
-        if let Some(udp) = &self.udp_in {
-            if let Some(n) = &udp.sport {
-                len += n.len() as u16;
-            }
-            if let Some(n) = &udp.dport {
-                len += n.len() as u16;
-            }
-        }
-        if let Some(udp) = &self.udp_out {
-            if let Some(n) = &udp.sport {
-                len += n.len() as u16;
-            }
-            if let Some(n) = &udp.dport {
-                len += n.len() as u16;
-            }
-        }
-        len
-    }
-}
-
-#[derive(Debug, Parser)]
-struct Opt {
-    #[clap(short, long, default_value = "wlp1s0")]
-    iface: String,
-    #[clap(short, long, default_value = "/etc/sfw/sfwconfig.toml")]
-    config: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
-    let logfile = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{d(%Y-%m-%d %H:%M:%S)} - {l} - {f} - {m}\n",
-        )))
-        .build("/var/log/sfw.log")?;
+
+    let log_line_pattern = "{d(%Y-%m-%d %H:%M:%S)} - {l} - {f} - {m}\n";
+
+    let trigger_size = 2u64.pow(20) * 100; // 100 MiB
+    let trigger = Box::new(SizeTrigger::new(trigger_size));
+
+    let roller_pattern = "/var/log/sfw/log_{}.gz";
+    let roller_count = 5;
+    let roller_base = 1;
+    let roller = Box::new(
+        FixedWindowRoller::builder()
+            .base(roller_base)
+            .build(roller_pattern, roller_count)
+            .unwrap(),
+    );
+
+    let compound_policy = Box::new(CompoundPolicy::new(trigger, roller));
+
+    let log_file = RollingFileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(log_line_pattern)))
+        .build("/var/log/sfw/sfw.log", compound_policy)
+        .unwrap();
 
     let config = Config::builder()
-        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-        .build(Root::builder().appender("logfile").build(LevelFilter::Info))?;
+        .appender(Appender::builder().build("log_file", Box::new(log_file)))
+        .build(
+            Root::builder()
+                .appender("log_file")
+                .build(LevelFilter::Debug),
+        )
+        .unwrap();
 
     log4rs::init_config(config)?;
     // env_logger::init();
+
     // Bump the memlock rlimit. This is needed for older kernels that don't use the
     // new memcg based accounting, see https://lwn.net/Articles/837122/
     let rlim = libc::rlimit {
@@ -198,14 +151,36 @@ async fn main() -> Result<(), anyhow::Error> {
     );
     config_len = config_.len();
     _ = load_config(&mut bpf, &config_);
-    // let ring_buf = RingBuf::try_from(
-    //     bpf.take_map("CONBUF").expect("CONBUF ringbuffer is exits"),
-    // )?;
+    let mut new_connection = AsyncPerfEventArray::try_from(
+        bpf.take_map("NEW").expect("Map is Exist"),
+    )?;
+    let (new_send, mut new_rev) = tokio::sync::mpsc::channel(2000);
+    for cpu_id in online_cpus()? {
+        let new_send = new_send.clone();
+        let mut perf_buf = new_connection.open(cpu_id, None)?;
+        tokio::task::spawn(async move {
+            // let mut u_connection: std::collections::HashMap<u32, Instant> =
+            //     std::collections::HashMap::new();
+            let mut buf = vec![BytesMut::with_capacity(16); 2000];
+            loop {
+                let events = perf_buf
+                    .read_events(&mut buf)
+                    .await
+                    .expect("new conection event");
+                for event in buf.iter_mut().take(events.read) {
+                    let key =
+                        unsafe { &*(event.as_ptr() as *const Connection) };
+                    _ = new_send.send(key).await;
+                    // u_connection.insert(key.into_session(), Instant::now());
+                }
+            }
+            Ok::<_>(())
+        });
+    }
 
     let (tx, rx) = tokio::sync::watch::channel(false);
     let t = tokio::spawn(async move {
         let mut rx = rx.clone();
-        // let mut async_fd = AsyncFd::new(ring_buf).unwrap();
         let mut interval_2 = interval(Duration::from_millis(10));
         // let mut conn: std::collections::HashMap<u32, Instant> =
         //     std::collections::HashMap::with_capacity(262_144);
@@ -214,27 +189,18 @@ async fn main() -> Result<(), anyhow::Error> {
 
         loop {
             tokio::select! {
-                // _ = async_fd.readable_mut() => {
-                //     let mut guard = async_fd.readable_mut().await.unwrap();
-                //     let rb = guard.get_inner_mut();
-                //
-                //     while let Some(read) = rb.next() {
-                //         let data = read.as_ptr();
-                //         let contrack = unsafe { std::ptr::read_unaligned::<Connection>(data as *const Connection) };
-                //         debug!("{:#?}", contrack);
-                //
-                //
-                //     }
-                //     guard.clear_ready();
-                //
-                // }
+                _ = new_rev.recv() => {
+                    if let Some(rb) = new_rev.recv().await {
+                        debug!("{:#?}", rb);
+                    }
+                }
 
 
                 _ = rx.changed() => {
-                        if *rx.borrow() {
-                            break;
-                        }
+                    if *rx.borrow() {
+                        break;
                     }
+                }
                 // _ = interval_1.tick() => {
                 //     let con = del_rev.try_recv();
                 //     if con.is_ok() {
@@ -280,7 +246,7 @@ async fn main() -> Result<(), anyhow::Error> {
     tx.send(true).unwrap();
     info!("Clearing task");
     // wait task to done
-    let mut bpf = t.await.unwrap().expect("bpf returned");
+    let mut bpf = t.await?.expect("bpf returned");
     let program: &mut Xdp = bpf.program_mut("sfw").unwrap().try_into()?;
     if program.detach(xdp_link).is_ok() {
         info!("detached xdp program");
@@ -288,119 +254,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let program: &mut SchedClassifier =
         bpf.program_mut("sfw_egress").unwrap().try_into()?;
     if program.detach(tc_link).is_ok() {
-        info!("detached xdp program");
+        info!("detached TC program");
     }
     info!("Exiting...");
-    Ok(())
-}
-
-fn load_config(bpf: &mut Bpf, config: &AppConfig) -> Result<(), anyhow::Error> {
-    if let Some(dns) = &config.dns {
-        let mut dns_list: HashMap<_, u32, u8> =
-            HashMap::try_from(bpf.map_mut("DNS_ADDR").unwrap())?;
-        for k in dns {
-            let ip_addrs: Ipv4Addr = k.parse().unwrap();
-            let addrs: u32 = ip_addrs.to_bits();
-            info!("allowed DNS IP: {:}", ip_addrs.to_string());
-            _ = dns_list.insert(addrs, 0x1, 0);
-        }
-    }
-    if let Some(tcp) = &config.tcp_in {
-        if let Some(n) = &tcp.sport {
-            let mut tcp_in_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("TCP_IN_SPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = tcp_in_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow incomming from tcp port: {:?}", port);
-                tcp_in_port.insert(port, 1u8, 0)?;
-            }
-        }
-        if let Some(n) = &tcp.dport {
-            let mut tcp_in_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("TCP_IN_DPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = tcp_in_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow incomming to tcp port: {:?}", port);
-                tcp_in_port.insert(port, 1u8, 0)?;
-            }
-        }
-    }
-    if let Some(tcp) = &config.tcp_out {
-        if let Some(n) = &tcp.sport {
-            let mut tcp_out_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("TCP_OUT_SPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = tcp_out_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow outgoing from tcp port: {:?}", port);
-                tcp_out_port.insert(port, 1u8, 0)?;
-            }
-        }
-        if let Some(n) = &tcp.dport {
-            let mut tcp_out_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("TCP_OUT_DPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = tcp_out_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow outgoing to tcp port: {:?}", port);
-                tcp_out_port.insert(port, 1u8, 0)?;
-            }
-        }
-    }
-    if let Some(udp) = &config.udp_in {
-        if let Some(n) = &udp.sport {
-            let mut udp_in_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("UDP_IN_SPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = udp_in_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow incomming from udp port {:?}", port);
-                udp_in_port.insert(port, 1u8, 0)?;
-            }
-        }
-        if let Some(n) = &udp.dport {
-            let mut udp_in_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("UDP_IN_DPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = udp_in_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow incomming to udp port {:?}", port);
-                udp_in_port.insert(port, 1u8, 0)?;
-            }
-        }
-    }
-    if let Some(udp) = &config.udp_out {
-        if let Some(n) = &udp.sport {
-            let mut udp_out_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("UDP_OUT_SPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = udp_out_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow outgoing from udp port {:?}", port);
-                udp_out_port.insert(port, 1u8, 0)?;
-            }
-        }
-        if let Some(n) = &udp.dport {
-            let mut udp_out_port: HashMap<_, u16, u8> =
-                HashMap::try_from(bpf.map_mut("UDP_OUT_DPORT").unwrap())?;
-            for port in 0..=65535 {
-                _ = udp_out_port.remove(&port);
-            }
-            for port in n {
-                info!("Allow outgoing to udp port {:?}", port);
-                udp_out_port.insert(port, 1u8, 0)?;
-            }
-        }
-    }
-    info!("Done parsing config!");
     Ok(())
 }
